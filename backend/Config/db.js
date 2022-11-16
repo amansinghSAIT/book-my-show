cd4b0e1966ea24578f5b3a3680ee8882e4e5c2f6
Aryan Dev <guptasaurabh946@gmail.com>
2022-11-16 22:23:59 +0530

    Delete db.js

--- backend/Config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
-const connection = mongoose.connect(
-  `mongodb+srv://nobody:${process.env.MONGO_PASSWORD}@cluster0.il2clvz.mongodb.net/todo`
-);
-
-module.exports = { connection };
